e87c4d808e41e504dc95d324fddd483abb1d674a
Pookoloco <90471681+Pookoloco@users.noreply.github.com>
2021-10-11 19:23:31 +0200

    Add files via upload

---
+++ Opdracht_01/script.js
@@ -0,0 +1,20 @@
+console.log("Hello Winc Academy")
+let name = 'Niels';
+console.log(name);
+console.log(4+4)
+console.log("4"+"4")
+console.log(12+7)
+console.log(19-12)
+console.log(7*3)
+console.log(28/4)
+console.log(22%7)
+let age = 38
+console.log(typeof age)
+let agetext = 'achtendertig'
+console.log(typeof agetext)
+
+
+
+
+
+
